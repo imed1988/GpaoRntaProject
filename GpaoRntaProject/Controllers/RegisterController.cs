e3c7a778fd61c02ae128f5ae22ae8866749aae2e
Imed Eddine Benzarti <i.benzarti@root.sa>
2018-11-15 00:58:21 +0100

    new project

---
+++ GpaoRntaProject/Controllers/RegisterController.cs
@@ -0,0 +1,34 @@
+﻿using GpaoRntaProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GpaoRntaProject.Controllers
+{
+    public class RegisterController : Controller
+    {
+        DBEntities db = new DBEntities();
+
+        // GET: Register
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+
+
+      
+        public JsonResult InsertUser(SiteUser user)
+        {
+            db.SiteUser.Add(user);
+            db.SaveChanges();
+            return Json("success", JsonRequestBehavior.AllowGet);
+        }
+
+
+
+
+    }
+}
