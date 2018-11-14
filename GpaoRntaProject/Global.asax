84af7db2ee60695214201c987af943c749ba6f6e
Imed Eddine Benzarti <i.benzarti@root.sa>
2018-11-14 19:02:53 +0100

    Ajoutez des fichiers projet.

---
+++ GpaoRntaProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GpaoRntaProject.MvcApplication" Language="C#" %>
---
+++ GpaoRntaProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GpaoRntaProject.Startup))]
+namespace GpaoRntaProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
